33082c214a2b22971b14cc274e7acf783f9d2e8d
Bryan Troxclair <bt0220l@gmail.com>
2019-02-06 15:48:28 -0600

    Changed static return values to use sensor readings

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const sensor = require('node-dht-sensor');
 
 app.get('/temperature', function(req, res) {
-	res.send('24 °C');
+  sensor.read(22, 4, function (err, temperature, 
+    humidity) {
+        if (!err) {
+          res.send(temperature.toFixed(1) + '°C');
+        }
+      });
 });
 
 app.get('/humidity', function(req, res) {
-	res.send('48%');
+  sensor.read(22, 4, function (err, temperature, 
+    humidity) {
+        if (!err) {
+         	res.send(humidity.toFixed(1) + '%');
+        }
+      });
 });
 
 app.listen(3000, function(){
