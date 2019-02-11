05dbd750f57c49f3f61ec9319e57dce099ece0a5
Bryan Troxclair <bt0220l@gmail.com>
2019-02-11 11:48:28 -0600

    Fetching sensor readings using XHR

--- server/public/script.js
+++ server/public/script.js
@@ -1,1 +1,29 @@
-console.log('Executing client side javascript...')
+console.log('Executing client side javascript...');
+
+/*
+     The fetch API uses a promise based syntax. It may look a  
+    little weird if you're seeing it for the first time, but    
+    it's an improvement over callbacks.
+    */
+
+    /*
+    First, we instantiate the first promise, which call the 
+    API at /temperature of our server
+    */
+    fetch('/temperature')
+    .then(results => {
+      /*
+      results.text() returns another promise, which resolves 
+    to the text response we receive from the API
+      */
+      return results.text()
+    })
+    .then(text => {
+      /*
+       This "text" variable is the response that the server 
+    gives us. Logging it on the console will show you " 
+      <strong>10.0</strong>°C"
+      */
+      console.log(text)
+    });
+    
