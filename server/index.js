3c0abdcde88605a3c75979f6d8e04c99a4fcd7da
Bryan Troxclair <bt0220l@gmail.com>
2019-02-10 21:38:32 -0600

    Caching our results

--- server/index.js
+++ server/index.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const app = express();
-const sensor = require('node-dht-sensor');
-const getSensorReadings = require('./get-sensor-readings');
+const getCachedSensorReadings = require('./get-cached-sensor-readings');
 
 app.get('/temperature', function(req, res) {
-  getSensorReadings((err, temperature, humidity) => {
-        if (!err) {
-          res.send(temperature.toFixed(1) + '°C');
-        }
-      });
+  res.send(getCachedSensorReadings.getTemperature().toFixed(1) + '°C');
 });
 
 app.get('/humidity', function(req, res) {
-  getSensorReadings((err, temperature, humidity) => {
-        if (!err) {
-         	res.send(humidity.toFixed(1) + '%');
-        }
-      });
+  res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%');
 });
 
 app.listen(3000, function(){
